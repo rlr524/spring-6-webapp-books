78bc11fb9ed7ce376e591738dbffd26ee19d689c
Rob Ranf <rob@emiyaconsulting.com>
2024-07-13 12:32:34 -0700

    tostring equals and hash functions

--- src/main/java/com/emiyaconsulting/spring6bookswebapp/domain/Author.java
+++ src/main/java/com/emiyaconsulting/spring6bookswebapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.emiyaconsulting.spring6bookswebapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -45,5 +46,29 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
